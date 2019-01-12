b4677746bb0c34641ad132944b0c9586cd235e10
lihong <564434750@qq.com>
2019-01-12 17:42:56 +0800

    上传项目

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@ComponentScan
+@EnableAutoConfiguration
+@EnableTransactionManagement
+@SpringBootApplication
+@EnableCaching
+public class DemoApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DemoApplication.class);
+    }
+}
+
---
+++ src/main/java/com/example/demo/dao/SysUserRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.dao;
+
+import com.example.demo.entity.SysUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SysUserRepository extends JpaRepository<SysUser, Long> {
+    SysUser findByUsername(String username);
+}
